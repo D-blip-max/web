215e56f782906b2911a0083946a0f2e6ec7f2023
dolan <dylancossioaguilera@gmail.com>
2026-09-18 11:52:20 -0400

    ya estan la url railway pa mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Reemplazar con la URL pública generada por Railway (ej. 'https://mi-backend.up.railway.app/api/v1')
-  apiUrl: 'http://localhost:8000/api/v1'
+  apiUrl: 'https://backend-production-d7d5d.up.railway.app/api/v1'
 };
